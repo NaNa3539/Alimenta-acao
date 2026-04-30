74422a4fe740b00c842671bf7474868dd144bb26
NaNa3539 <nanamacedu@gmail.com>
2026-04-30 17:34:07 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ app.use(express.static('public'));
 
 // fornecer alimentos em JSON
 app.get('/api/alimentos', (req, res) => {
-  fs.readFile('data/alimentos.json', 'utf8', (err, data) => {
+  fs.readFile('public/alimentos.json', 'utf8', (err, data) => {
     if (err) {
       res.status(500).json({ error: 'Erro ao ler o arquivo de alimentos' });
       return;
